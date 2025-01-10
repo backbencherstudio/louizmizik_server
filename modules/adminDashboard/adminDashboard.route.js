f34077d0ae481c5bdb8adbb875896cdb4c8684ca
Nafiz Al Turabi <nafizalturabi18cse@gmail.com>
2025-01-10 16:02:52 +0600

    admin dashboard

--- modules/adminDashboard/adminDashboard.route.js
+++ modules/adminDashboard/adminDashboard.route.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { adminDashboard, addUserBlacklist, allUserDetails, getUserRegistrationsByTimeframe, revenueDashboard, AllTransections } = require("./adminDashboard.controllar");
+const { verifyUser } = require("../../middleware/verifyUser");
 
 
 const router = express.Router();
@@ -8,9 +9,9 @@ const router = express.Router();
 router.get("/adminDashboard", adminDashboard);
 router.get("/revenueDashboard", revenueDashboard)
 // here will be add varify Admin
-router.delete("user-blacklist/:userId" , addUserBlacklist)
+router.delete("user-blacklist/:userId" , verifyUser, addUserBlacklist)
 
 router.get("/allUserDetails", allUserDetails)
-router.get("/user-registrations", getUserRegistrationsByTimeframe)
+router.get("/users", getUserRegistrationsByTimeframe)
 router.get("/AllTransections", AllTransections)
 module.exports = router;
